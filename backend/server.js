7f07a412928f2a622fd00f0589778fc6b3b5c6f0
Sanj33d <144790238+Sanj33d@users.noreply.github.com>
2026-02-27 10:50:08 +0600

    server connected to mongo

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,42 @@ const port = process.env.PORT || 5000;
 
 const cors = require('cors');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// mongodb new DNS issue solution: 
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+
+// mongodb user: GreenMartDb
+// pass: xXlIOpkBFnRDTKiM
+
+const uri = "mongodb+srv://GreenMartDb:xXlIOpkBFnRDTKiM@cluster0.ykbg5m6.mongodb.net/?appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('GreenMart server is running');
